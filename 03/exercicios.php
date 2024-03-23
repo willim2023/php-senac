5e378aac81aac13b48d084fc3a16214c0a0e4049
willim2023 <paul.willim.2018@gmail.com>
2024-03-22 22:22:16 -0300

    exercicios

---
+++ 03/exercicios.php
@@ -0,0 +1,53 @@
+<?php
+    //Exercício 1 - Soma:
+    //Declare duas variáveis contendo números inteiros e exiba a soma dos dois números.
+
+    //Exercício 2 - Subtração:
+    //Declare duas variáveis contendo números inteiros e exiba a diferença entre eles.
+
+    //Exercício 3 - Multiplicação:
+    //Declare duas variáveis contendo números inteiros e exiba o produto dos dois números.
+
+    //Exercício 4 - Divisão:
+    //Declare duas variáveis contendo números inteiros e exiba o resultado da divisão do primeiro pelo segundo.
+
+    //Exercício 5 - Módulo (Resto da Divisão):
+    //Declare duas variáveis contendo números inteiros e exiba o resto da divisão do primeiro pelo segundo.
+
+    //Exercício 6 - Incremento e Decremento:
+    //Declare uma variável contendo um número inteiro. Em seguida, incremente o valor dessa variável em 1 e exiba o resultado. Depois, decremente o valor da variável em 1 e exiba o resultado.
+
+    //Exercício 7 - Operadores de Comparação:
+    //Declare duas variáveis contendo números inteiros e utilize operadores de comparação para determinar se o primeiro número é igual, diferente, maior, menor, maior ou igual ou menor ou igual ao segundo número. Exiba o resultado de cada comparação.
+
+    //ex 01
+    $n1 = 22;
+    $n2 = 33;
+
+    //echo "Valor A = $n1 e valor B = $n2 <br>";
+    $soma = $n1 + $n2;
+    echo "Soma: $soma <br><br>";
+
+    //ex 02
+    $num1 = 40;
+    $num2 = 15;
+    $subtracao = $num1 - $num2;
+    echo "Subtração: $subtracao <br><br>";
+
+    //ex 03
+    $num3 = 4;
+    $num4 = 10;
+    $multiplicacao = $num3 * $num4;
+    echo "Multiplicação: $multiplicacao <br><br>";
+
+    //ex 04 
+    $num5 = 50;
+    $num6 = 5;
+    $divisao = $num5 / $num6;
+    echo "Divisão: $divisao <br><br>";
+
+    //ex 05 
+    $modulo = $num5 % $num6;
+    echo "Módulo: $modulo <br><br>";
+
+?>
