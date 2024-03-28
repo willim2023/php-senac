27d4ab041cb78b2fe9dfd213d2d8ae7392f9e8e1
willim2023 <paul.willim.2018@gmail.com>
2024-03-28 19:48:13 -0300

    Aula Break e Continue - Finalizado.

--- 08/index.php
+++ 08/index.php
@@ -13,7 +13,7 @@ echo "Exemplo 2: while <br>";
 
 $contador = 1;
 
-while($contador <= 5) {
+while ($contador <= 5) {
     echo "Numero $contador <br>";
     $contador++;
 }
@@ -30,7 +30,18 @@ echo "<br>";
 // Exemplo 4: FOREACH
 echo "Exemplo 4: FOREACH <br>";
 $array_vogais = ["a", "e", "i", "o", "u"];
-foreach($array_vogais as $vogal) {
+foreach ($array_vogais as $vogal) {
     echo "Vogal: $vogal <br>";
 }
-?>
+echo "<br>";
+
+// Exemplo 5: BREAK e CONTINUE
+echo "Exemplo 5: BREAK E CONTINUE <br>";
+for ($i = 1; $i <= 10; $i++) {
+    if ($i == 3) {
+        continue;
+        break;
+    }
+
+    echo "Número $i <br>";
+}
