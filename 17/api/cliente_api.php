4e8c46fa49a6c60b814b0992ad496a4aa1268745
willim2023 <paul.willim.2018@gmail.com>
2024-04-23 19:33:07 -0300

    Estrutura da conta API

--- 17/api/cliente_api.php
+++ 17/api/cliente_api.php
@@ -8,6 +8,18 @@ switch($action) {
     case 'listar':
         listarClientes();
         break;
+    case 'buscar':
+        buscarClientePorId();
+        break;
+    case 'cadastrar':
+        cadastrarCliente();
+        break;
+    case 'atualizar':
+        atualizarCliente();
+        break;
+    case 'excluir':
+        excluirCliente();
+        break;
     default:
         http_response_code(400); // Requisição inválida
         echo json_encode(['error' => 'Ação inválida']);
@@ -17,3 +29,63 @@ function listarClientes() {
     $clientes = ClienteRepository::getAllClientes();
     echo json_encode($clientes);
 }
+
+function buscarClientePorId() {
+    if($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $id = $_GET['id'];
+        $cliente = ClienteRepository::getClienteById($id);
+
+        if($cliente) {
+            echo json_encode($cliente);
+        } else {
+            http_response_code(404); // Cliente não encontrado
+            echo json_encode(['error' => 'Cliente não encontrado']);
+        }
+    } else {
+        http_response_code(405); // Método não permitido
+    }
+}
+
+function cadastrarCliente() {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"));
+        $nome = $data->nome;
+        $cpf = $data->cpf;
+
+        $success = ClienteRepository::insertCliente($nome, $cpf);
+        echo json_encode(['success' => $success]);
+    } else {
+        http_response_code(405);
+    }
+}
+
+function atualizarCliente() {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"));
+        $id = $data->id;
+        $nome = $data->nome;
+        $cpf = $data->cpf;
+
+        $success = ClienteRepository::updateCliente($id, $nome, $cpf);
+        echo json_encode(['success' => $success]);
+    } else {
+        http_response_code(405);
+    }
+}
+
+function excluirCliente() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"));
+        $id = $data->id;
+
+        $success = ClienteRepository::deleteCliente($id);
+        echo json_encode(['success' => $success]);
+    } else {
+        http_response_code(405);
+    }
+}
+// Create Read Update Delete
+// Create = POST (INSERT)
+// Read = GET (SELECT)
+// Update = POST(GET) (UPDATE SET)
+// Delete = POST (DELETE)
--- 17/api/conta_api.php
+++ 17/api/conta_api.php
@@ -5,44 +5,49 @@ $action = $_GET['action'];
 
 switch($action) {
     case 'listar':
-        listarClientes();
+        listarContas();
         break;
         case 'buscar':
-            buscarClientePorId();
+            buscarContaById();
             break;
         case 'cadastrar':
-            cadastrarCliente();
+            cadastrarConta();
             break;
         case 'atualizar':
-            atualizarCliente();
+            atualizarConta();
+            break;
         case 'excluir':
-            excluirCliente();
+            excluirConta();
             break;
     default:
         http_response_code(400); // Requisição inválida
         echo json_encode(['error' => 'Ação inválida']);
 }
 
-function listarClientes() {
+function listarContas() {
     $contas = ContaRepository::getAllContas();
-        echo json_encode($contas);
+    echo json_encode($contas);
+}
+
+function buscarContaById () {
     
 }
 
-function buscarClientePorId() {
+function  cadastrarConta() {
+     
+}
+
+function  atualizarConta() { 
+     
+}
+
+function excluirConta() {
 
 }
 
-function cadastrarCliente() {
-
-}
-
-function atualizarCliente() {
-
-}
-
-function excluirCliente() {
-
-}
-
+// Create Read Update Delete
+// Create = POST (INSERT)
+// Read = GET (SELECT)
+// Update = POST(GET) (UPDATE SET)
+// Delete = POST (DELETE)
 ?>
